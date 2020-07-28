da372509dd7525dd732b4c875429200e7ec481cf
Vikas Gupta <vikas.gupti047@gmail.com>
2020-07-28 15:06:49 +0530

    line-through

--- src/ToDoItems.js
+++ src/ToDoItems.js
@@ -1,10 +1,15 @@
 import React from "react"
 
 function Todoitem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "efefef",
+        textDecoration: "line-through"
+    }
     return(
         <div className="todo-item">
             <input type="checkbox" checked={props.item.completed} onChange={() => props.handleChange(props.item.id)}/>
-            <p>{props.item.text}</p>
+            <p style={props.item.completed ? completedStyle : null}>{props.item.text}</p>
         </div>
     )
      
