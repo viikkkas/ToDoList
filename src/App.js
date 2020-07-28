83a66c24af9e76702ec4c63b9760a2acf9cc22a8
Vikas Gupta <vikas.gupti047@gmail.com>
2020-07-28 13:20:49 +0530

    state change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Todoitem from './ToDoItems';
 import './style.css'
@@ -11,10 +10,29 @@ class App extends React.Component {
     this.state = {
       todos: todoData
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    console.log("Ass",id)
+    this.setState(prevState => {
+      const updatedtodos = prevState.todos.map(todoo => {
+        if(todoo.id === id){
+          return{
+            ...todoo,
+            completed: !todoo.completed
+          }
+        }
+        return todoo
+      })
+      return {
+        todos: updatedtodos
+      }
+    })
   }
 
   render(){
-    const todo = this.state.todos.map(item => <Todoitem key={item.id} item={item}/>)
+    const todo = this.state.todos.map(item => <Todoitem key={item.id} item={item} handleChange={this.handleChange}/>)
     return (
       <div className="todo-list">
         {todo}
--- src/ToDoItems.js
+++ src/ToDoItems.js
@@ -3,11 +3,11 @@ import React from "react"
 function Todoitem(props) {
     return(
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed}/>
+            <input type="checkbox" checked={props.item.completed} onChange={() => props.handleChange(props.item.id)}/>
             <p>{props.item.text}</p>
         </div>
     )
-    
+     
 }
 
-export default Todoitem;
+export default Todoitem
